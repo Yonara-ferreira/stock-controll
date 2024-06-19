a5a789ac048d1e6d6c3f735141257ea01169a566
Yonara Ferreira <104727934+Yonara-ferreira@users.noreply.github.com>
2024-06-18 23:49:20 -0300

    Feat: function delete cookie and redirect home

--- src/app/shared/components/toolbar-navigation/toolbar-navigation.component.ts
+++ src/app/shared/components/toolbar-navigation/toolbar-navigation.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-toolbar-navigation',
   templateUrl: './toolbar-navigation.component.html',
   styleUrls: [],
 })
-export class ToolbarNavigationComponent {}
+export class ToolbarNavigationComponent {
+  constructor(private cookie: CookieService, private router: Router) {}
+
+  handleLogout(): void {
+    this.cookie.delete('USER_INFO');
+    console.log('Coockie deleted:', this.cookie.get('USER_INFO')); // Verificar se o cookie foi realmente deletado
+    this.router.navigate(['/home']).then(() => {});
+  }
+}
