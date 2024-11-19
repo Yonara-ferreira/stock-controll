669a083f7328b3ea8c5f0266806ee8e6ffe9733d
Yonara Ferreira <104727934+Yonara-ferreira@users.noreply.github.com>
2024-11-18 23:45:31 -0300

    Created module for categories

---
+++ src/app/modules/categories/categories.module.ts
@@ -0,0 +1,49 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { ConfirmationService } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { DropdownModule } from 'primeng/dropdown';
+import { DialogService, DynamicDialogModule } from 'primeng/dynamicdialog';
+import { InputMaskModule } from 'primeng/inputmask';
+import { InputSwitchModule } from 'primeng/inputswitch';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { TableModule } from 'primeng/table';
+import { ToolbarModule } from 'primeng/toolbar';
+import { TooltipModule } from 'primeng/tooltip';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { CATEGORIES_ROUTES } from './categories.routing';
+import { CategoriesHomeComponent } from './page/categories-home/categories-home.component';
+
+@NgModule({
+  declarations: [CategoriesHomeComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forChild(CATEGORIES_ROUTES),
+    SharedModule,
+    HttpClientModule,
+    // prime ng
+    CardModule,
+    ButtonModule,
+    ToolbarModule,
+    TableModule,
+    InputMaskModule,
+    InputSwitchModule,
+    InputTextModule,
+    InputTextareaModule,
+    DynamicDialogModule,
+    TooltipModule,
+    DropdownModule,
+    ConfirmDialogModule,
+
+  ],
+  providers: [DialogService, ConfirmationService]
+})
+export class CategoriesModule {}
